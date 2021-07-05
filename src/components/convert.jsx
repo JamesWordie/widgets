0f8b31ffea26d29a3df522313854360cd2d9ab08
James Wordie <james.wordie@yahoo.com>
2021-07-05 16:49:56 +0100

    got basic search and return of results working

--- src/components/convert.jsx
+++ src/components/convert.jsx
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Convert = ({ text, language }) => {
+  const [translatedText, setTranslatedText] = useState('');
+
   useEffect(() => {
-    console.log('new language or text')
+    const doTranslation = async () => {
+      const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: text,
+          target: language.value,
+          key: process.env.REACT_APP_API_KEY
+        }
+      }
+    );
+
+    setTranslatedText(data.data.translations[0].translatedText)
+  };
+
+  doTranslation();
   }, [language, text]);
 
   return <div />
